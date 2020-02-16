2938762a07d738f6866f7ee3d5cf3bbf32be42fc
dmitrii_moiseev <msustudent@yandex.ru>
2020-02-16 18:32:38 +0600

    final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
+/*
+    Добрый день! Класс Main существует просто так :) набор в тестов найдете в SeparatorTest,
+    реализацию задачи в классе Separator.
+ */
+
+
 public class Main {
     public static void main(String[] args) throws SeparatorException {
-        //int a[] = {4,5,6,7,8,9,12};
-        //int a[] = {4,5,6,7,8,9};
-        //int a[] = {1,1,1,1,1,1,1,2};
-        //int a[] = {2,1,1};
         int a[] = {1,1,1,1,1,1,1,1,1,1,9,9,2};
         List<List<Integer>> result = Separator.separate(a, 3);
         if(result.size() != 0) {
